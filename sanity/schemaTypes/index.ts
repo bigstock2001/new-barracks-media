073107322a44650f0c78e08ab45a46523ac830ac
bigstock2001 <donald.l.dunn@gmail.com>
2026-01-15 15:48:13 -0600

    updated sanity files

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,13 +1,12 @@
-import type { SchemaTypeDefinition } from "sanity";
+// sanity/schemaTypes/index.ts
+import { type SchemaTypeDefinition } from "sanity";
+
 import service from "./service";
 import networkMetrics from "./networkMetrics";
 
-export const schemaTypes: SchemaTypeDefinition[] = [
-  service,
-  networkMetrics as SchemaTypeDefinition,
-];
-
-// ✅ sanity.config.ts expects this export name
-export const schema = {
-  types: schemaTypes,
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    service,
+    networkMetrics,
+  ],
 };
