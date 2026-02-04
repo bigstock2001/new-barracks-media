d2149aa37636451339855d716992a518990bbb67
bigstock2001 <donald.l.dunn@gmail.com>
2026-02-04 14:32:08 -0600

    update

---
+++ app/admin/announcements/page.jsx
@@ -0,0 +1,8 @@
+export default function AdminAnnouncementsPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Admin: Announcements</h1>
+      <p>Route is working: /admin/announcements</p>
+    </main>
+  );
+}
