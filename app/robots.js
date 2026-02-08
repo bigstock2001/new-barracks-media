f175485cda3000a1e24853055ee0522996f33013
bigstock2001 <donald.l.dunn@gmail.com>
2026-02-07 21:47:15 -0600

    update

---
+++ app/robots.js
@@ -0,0 +1,12 @@
+// app/robots.js
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://barracksmedia.com/sitemap.xml",
+  };
+}
